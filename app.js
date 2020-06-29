6dd6a20be241593887c4628341056f5d0437b44b
Harshad Kapei <harshad.kapei@wohlig.com>
2020-06-29 23:14:07 +0530

    incresed process memory added

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const mongoose = require("mongoose");
 const json2xls = require("json2xls");
+const v8 = require("v8");
+console.log(
+  "total_available_size",
+  (v8.getHeapStatistics().total_available_size / 1024 / 1024 / 1024).toFixed(
+    2
+  ) + "~GB"
+);
 
 mongoose.connect(
   env.mongoUrl + "jsontoxl",
